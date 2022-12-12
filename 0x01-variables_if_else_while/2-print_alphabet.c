35748f3482b4e6c80dfe863f5806ffa2c631ccf5
adjei rossi richard <rossiadjei70@gmail.com>
2022-12-12 06:29:48 -0800

    alphabt game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char apht;
+
+	for (apht = 'a' ; apht <= 'z' ; apht++)
+	{
+		putchar(apht);
+	}
+	return (0);
+}
