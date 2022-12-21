7d8f9b0406ae48aaeb92f1f1f27e70bdc1b96d99
adjei rossi richard <rossiadjei70@gmail.com>
2022-12-21 04:38:07 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcat - appends strings
+ * @dest: destination to append
+ * @src: what to append
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index, j;
+
+	for (index = 0; dest[index] != '\0'; index++)
+	{
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[index] = src[j];
+		j++;
+		index++;
+	}
+	index++;
+	dest[index] = '\0';
+	return (dest);
+}
