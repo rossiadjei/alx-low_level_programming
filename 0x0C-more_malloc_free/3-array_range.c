625aa974f1c80a44c736b057ef329a464c9b3894
adjei rossi richard <rossiadjei70@gmail.com>
2023-01-12 15:16:56 -0800

    Task 3 hot fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,14 +11,13 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i;
-	int size;
+	int i, size;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	size = ((max-min) + 1);
+	size = max - min + 1;
 	ptr = malloc(size * sizeof(int));
 	if (ptr == NULL)
 	{
