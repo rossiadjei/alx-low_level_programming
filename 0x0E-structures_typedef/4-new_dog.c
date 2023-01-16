6dddb77200e34288834b7ed45fb46d71f11f7c26
adjei rossi richard <rossiadjei70@gmail.com>
2023-01-16 15:07:34 -0800

    Task 4 re-implementation to fix errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,79 +1,89 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
- * *_strcpy - copy string
- * @dest: char to test
- * @src: char to test
+ * new_dog -  creates anew dog, memory allocated
+ * @name: name
+ * @age: age
+ * @owner: owner
  *
- * Return: char
+ * Return: pointer to new dog_t
  */
-char *_strcpy(char *dest, char *src)
-{
-int i;
 
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
-dest[i] = '\0';
-return (dest);
-
-}
-/**
- * new_dog - creates a new dog
- * @name : name of dog
- * @age : age of dog
- * @owner : owner of dog
- *
- * Return - Return a pointer to the structure
- */
-char *_strcpy(char *dest, char *src)
-{
-int i;
-
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-
-dest[i] = '\0';
-return (dest);
-}
-
-/**
- * new_dog - Creates a new structure dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- * Return: Return a pointer to the structure
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
+	dog_t *new_dog;
+	char *namemem, *ownermem;
+	int size_name, size_owner;
 
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	(*ptr).name = malloc(sizeof(name) + 1);
-	if (name == NULL)
+	size_name = _strlen(name) + 1;
+	size_owner = _strlen(owner) + 1;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	namemem = malloc(size_name * sizeof(char));
+	if (namemem == NULL)
 	{
-		free(ptr);
+		free(new_dog);
 		return (NULL);
 	}
+	_strcpy(namemem, name);
 
-	(*ptr).owner = malloc(sizeof(owner) + 1);
-	if ((*ptr).owner == NULL)
+	ownermem = malloc(size_owner * sizeof(char));
+	if (ownermem == NULL)
 	{
-		free((*ptr).name);
-		free(ptr);
+		free(namemem);
+		free(new_dog);
 		return (NULL);
 	}
+	_strcpy(ownermem, owner);
 
-	_strcpy((*ptr).name, name);
-	_strcpy((*ptr).owner, owner);
-	(*ptr).age = age;
+	new_dog->name = namemem;
+	new_dog->age = age;
+	new_dog->owner = ownermem;
+	return (new_dog);
+}
 
-	return (ptr);
+/**
+ *_strlen - prints the length of a string
+ *@s: string variable
+ *Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+		i++;
+	return (i);
+}
+
+/**
+ *_strcpy - copies string
+ *
+ *@src: source string
+ *@dest: destination string
+ *
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while ((*(dest + i) = *src++))
+	{
+		i++;
+	}
+	return (dest);
+
 }
