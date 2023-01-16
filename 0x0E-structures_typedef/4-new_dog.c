e3b7f510f53de731b7bf3d02430bf8416254a4de
adjei rossi richard <rossiadjei70@gmail.com>
2023-01-16 15:12:28 -0800

    hot fix task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
-
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 /**
  * new_dog -  creates anew dog, memory allocated
  * @name: name
@@ -9,9 +10,6 @@
  *
  * Return: pointer to new dog_t
  */
-
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
