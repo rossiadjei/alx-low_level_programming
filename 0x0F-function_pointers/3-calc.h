330553de8c83e844b2111b2c432cffef06b19a2b
adjei rossi richard <rossiadjei70@gmail.com>
2023-01-18 17:03:23 -0800

    Hot fix for task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,10 +1,15 @@
-ifndef _CALC_H_
-#define _CALC_H_
+#ifndef CALC_H
+#define CALC_H
+
+/*
+ * Desc: Header file containing all structures and
+ *       prototypes used by the 3-main.c program.
+ */
+
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
 typedef struct op
 {
